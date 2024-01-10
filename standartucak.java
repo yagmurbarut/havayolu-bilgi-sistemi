6e2417d5efa73430fe626876a05f4c22a205f6c5
Yağmur Barut <119848166+yagmurbarut@users.noreply.github.com>
2024-01-10 20:26:31 +0300

    Add files via upload

---
+++ standartucak.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personel;
+
+/**
+ *
+ * @author yağmur
+ */
+public class standartucak extends ucak{
+    public standartucak(){
+        super("standart uçak", 200, 80, 20, 100, 10);
+    }
+}
