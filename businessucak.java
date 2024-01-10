8a33c907a18a7647e60bd35fac4aa8806e639ec9
Yağmur Barut <119848166+yagmurbarut@users.noreply.github.com>
2024-01-10 20:24:26 +0300

    Add files via upload

---
+++ businessucak.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personel;
+
+/**
+ *
+ * @author yağmur
+ */
+public class businessucak extends ucak{
+    public businessucak(){
+        super("business ucak", 100, 20, 50, 30, 12);
+    }
+}
