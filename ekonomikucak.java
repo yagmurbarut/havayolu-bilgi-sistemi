1f8a998c90fb1af00eec79f026bafc3d56bd54cb
Yağmur Barut <119848166+yagmurbarut@users.noreply.github.com>
2024-01-10 20:24:39 +0300

    Add files via upload

---
+++ ekonomikucak.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personel;
+
+/**
+ *
+ * @author yağmur
+ */
+public class ekonomikucak extends ucak{
+    public ekonomikucak(){
+        super("ekonomik ucak", 200, 150, 20,30, 8);
+    }
+}
